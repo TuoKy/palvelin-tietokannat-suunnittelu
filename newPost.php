8f216c419812e4fdd8f0785c24163b1c98a21eef
TuoKy <tuomas.kytta@gmail.com>
2015-03-19 16:44:58 +0200

    raah
    sais toimia

--- newPost.php
+++ newPost.php
@@ -14,18 +14,16 @@
 			<label>Otsikko</label>
 			<input type="text" name="otsikko"><br />
 			<label>Sisältö</label>
-			<script>
-			$('#editor').wysiwyg();
-			</script>
-			    <div class="btn-toolbar" data-role="editor-toolbar"
-					data-target="#editor">    
-				</div> 
+			<div class="btn-toolbar" data-role="editor-toolbar"
+				data-target="#editor">    
+			</div> 
 			<button type="submit" name ="register" class="btn btn-default">Register</button>
 			<button type="submit" name ="cancel" class="btn btn-default">Cancel</button>
 			</form>
 		</div>	
 				
 		</div>
+
 </body>
 </html>
 
