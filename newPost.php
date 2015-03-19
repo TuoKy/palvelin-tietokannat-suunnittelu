54d8e3a7dd2a82039bfc4fea356ead09ab72aeaf
TuoKy <tuomas.kytta@gmail.com>
2015-03-20 01:55:46 +0200

    textarea
    Weeee

--- newPost.php
+++ newPost.php
@@ -3,27 +3,34 @@
 <head>
 	<title>NewPost</title>
 <?php include("head.txt");?>
- <script src="bootstrap-wysiwyg.js.js"></script> 
+ 
 </head>
 <!--Navbar-->
 <?php include("navbar.php");?>
 <div class="container">
 		<div class="content">
 			<div class="formContainer">
-			<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-			<label>Otsikko</label>
-			<input type="text" name="otsikko"><br />
-			<label>Sisältö</label>
-			<div class="btn-toolbar" data-role="editor-toolbar"
-				data-target="#editor">    
-			</div> 
-			<button type="submit" name ="register" class="btn btn-default">Register</button>
-			<button type="submit" name ="cancel" class="btn btn-default">Cancel</button>
-			</form>
-		</div>	
-				
-		</div>
+				<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+					<label>Otsikko</label>
+						<input type="text" name="otsikko"><br />
+					<label>Sisältö</label>
+						<div class="row-fluid">							
+							<div id="wysiwygEditor">
+								<div>
+									<textarea class="input-xlarge" name="editor" id="editor" style="width: 100%; height: 200px; outline: none;"></textarea>
+								</div>
+								<div style="margin-top: 20px;">
+									<button type="submit" name ="post" class="btn btn-default">Post</button>
+									<button type="submit" name ="cancel" class="btn btn-default">Cancel</button>
+								</div>
+				</form>
+							</div>
+							
 
+						</div>
+			</div>					
+		</div>
+</div>
 </body>
 </html>
 
