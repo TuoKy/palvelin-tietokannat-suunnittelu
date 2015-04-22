f9b919628ea558aaa8fcd707d5296c9a926129d9
Eetu Manninen <eetu94@live.fi>
2015-04-22 17:18:06 +0300

    Nyt vaaditaan asioita rekisteröityessä

--- register.php
+++ register.php
@@ -7,23 +7,22 @@
 		<div class="formContainer">
 			<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
 			<label>Name</label>
-			<input type="text" name="name" maxlength="20"><br />
+			<input type="text" name="name" maxlength="20" required><br />
 			<label>Password</label>
-			<input type="text" name="password" maxlength="32"><br />
+			<input type="text" name="password" maxlength="32" required><br />
 			<label>Email</label>
-			<input type="email" name="email" maxlength="32"><br />
+			<input type="email" name="email" maxlength="32" required><br />
 			<button type="submit" name ="register" class="btn btn-default">Register</button>
-			<button type="submit" name ="cancel" class="btn btn-default">Cancel</button>
 			</form>
 		</div>
 	</div>
 </div>			
 <?php
+
 if (isset($_POST['register']) AND isset($_POST['name']) AND isset($_POST['password'])){
-			$kayttajaNimi = htmlentities($_POST['name']);
-			$email = htmlentities($_POST['email']);
-			$salasana = htmlentities($_POST['password']);
-			
-			$dbTouch->luo_kayttaja($kayttajaNimi, $email, $salasana); 
+	$kayttajaNimi = htmlentities($_POST['name']);
+	$email = htmlentities($_POST['email']);
+	$salasana = htmlentities($_POST['password']);		
+	$dbTouch->luo_kayttaja($kayttajaNimi, $email, $salasana); 
 }
 ?>	
